06fac48aa524177595d5cf8cb259de76bf9000b3
Kent Fredric <kentfredric@gmail.com>
2013-12-07 09:22:31 +1300

    Less attributes, more methods, because git is external, and tags could well change due to external factors between steps, caching may be over-eager

--- lib/Dist/Zilla/Util/Git/Tags.pm
+++ lib/Dist/Zilla/Util/Git/Tags.pm
@@ -22,16 +22,12 @@ Namely, each tag returned is a tag object, and you can view tag properties with 
         zilla => $self->zilla
     );
 
-    my $tags = $tags_finder->tags;
-    for my $tag ( @{ $tags } ) {
+    for my $tag ( $tags_finder->tags ) {
         printf "%s - %s\n", $tag->name, $tag->sha1;
     }
 
 =cut
 
-=attr C<tags>
-
-An C<ArrayRef> of L<< C<::Tags::Tag> objects|Dist::Zilla::Util::Git::Tags::Tag >>
 
 =attr C<git>
 
@@ -43,16 +39,10 @@ Auto-Built from C<zilla> with L<< C<::Util::Git::Wrapper>|Dist::Zilla::Util::Git
 
 A Dist::Zilla instance. Mandatory unless you passed C<git>
 
-=attr C<tag_sha1_map>
-
-A C<HashRef> of C<< sha1 => [ tag , tag ] >> entries.
-
 =cut
 
-has 'tags'         => ( isa => ArrayRef =>, is => ro =>, lazy_build    => 1 );
 has 'git'          => ( isa => Object   =>, is => ro =>, lazy_build    => 1 );
 has 'zilla'        => ( isa => Object   =>, is => ro =>, lazy_required => 1 );
-has 'tag_sha1_map' => ( isa => HashRef  =>, is => ro =>, lazy_build    => 1 );
 
 sub _build_git {
   my ($self) = @_;
@@ -74,27 +64,46 @@ sub _mk_tags {
   return map { $self->_mk_tag($_) } @tags;
 }
 
-sub _build_tags {
-  my ($self) = @_;
-  return [ $self->_mk_tags( $self->git->tag ) ];
+=method C<tags>
+
+A C<List> of L<< C<::Tags::Tag> objects|Dist::Zilla::Util::Git::Tags::Tag >>
+
+    my @tags = $tag_finder->tags();
+
+=cut
+
+sub tags {
+    my ( $self ) = @_;
+    return $self->_mk_tags( $self->git->tag );
 }
 
-sub _build_tag_sha1_map {
+=method C<tag_sha1_map>
+
+A C<HashRef> of C<< sha1 => [ L<< tag|Dist::Zilla::Util::Git::Tags::Tag >>,  L<< tag|Dist::Zilla::Util::Git::Tags::Tag >> ] >> entries.
+
+    my $hash = $tag_finder->tag_sha1_map();
+
+=cut
+
+sub tag_sha1_map {
   my ($self) = @_;
 
-  my $hash = {};
-  for my $tag ( @{ $self->tags } ) {
+  my %hash ;
+  for my $tag ( $self->tags ) {
     my $sha1 = $tag->sha1;
-    if ( not exists $hash->{$sha1} ) {
-      $hash->{$sha1} = [];
+    if ( not exists $hash{$sha1} ) {
+      $hash{$sha1} = [];
     }
-    push @{ $hash->{$sha1} }, $tag;
+    push @{ $hash{$sha1} }, $tag;
   }
-  return $hash;
-
+  return \%hash;
 }
 
 =method C<tags_for_rev>
+
+
+A C<List> of L<< C<::Tags::Tag> objects|Dist::Zilla::Util::Git::Tags::Tag >> that point to the given C<SHA1>.
+
 
     $tag_finder->tags_for_rev( $sha1_or_commitish_etc );
 
