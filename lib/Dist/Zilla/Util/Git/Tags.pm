c1bf0eaf9b8dc3611daec234e3fc874bdf7aca28
Kent Fredric <kentfredric@gmail.com>
2013-12-09 18:08:54 +1300

    Build results of 06c9b13 (on master)

--- lib/Dist/Zilla/Util/Git/Tags.pm
+++ lib/Dist/Zilla/Util/Git/Tags.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::Util::Git::Tags::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Util::Git::Tags::VERSION = '0.001002';
+  $Dist::Zilla::Util::Git::Tags::VERSION = '0.002000';
 }
 
 # ABSTRACT: Extract all tags from a repository
@@ -120,7 +120,7 @@ Dist::Zilla::Util::Git::Tags - Extract all tags from a repository
 
 =head1 VERSION
 
-version 0.001002
+version 0.002000
 
 =head1 SYNOPSIS
 
